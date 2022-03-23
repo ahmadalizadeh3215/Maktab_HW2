0980f019b51a883fdaea222346202471433e7d03
ahmadalizadeh3215 <ahmadalizadem361218@gmail.com>
2022-03-24 00:10:25 +0430

    create main class

--- src/Library/Main.java
+++ src/Library/Main.java
@@ -1,4 +1,13 @@
 package Library;
 
+import Library.util.Menu;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Menu mainRun = new Menu();
+        System.out.println(" ***WELCOME***");
+        mainRun.initDependencies();
+        mainRun.mainMenuRun();
+    }
 }
